10cfc3d689c1ab0177a5262b71e9c5e939360207
name_snrl <Demogorgon-74@ya.ru>
2023-02-14 19:35:55 +0600

    delete qutebrowser cfgs

--- .config/qutebrowser/config.py
+++
@@ -1,7 +0,0 @@
-config.load_autoconfig(False)
-c.content.blocking.enabled = False
-c.statusbar.show = 'never'
-c.tabs.show = 'never'
-c.colors.webpage.bg = '#2b323b'
-
-c.colors.webpage.preferred_color_scheme = 'dark'
