0a13299670dfd15914587d0d8a4dd59facdcb3da
name_snrl <Demogorgon-74@ya.ru>
2023-10-20 20:10:47 +0500

    sway: anki script refactoring

--- .config/sway/scripts/anki.sh
+++ .config/sway/scripts/anki.sh
@@ -1,24 +1,22 @@
 #!/usr/bin/env bash
 
+notify() {
+    gdbus call --session \
+        --dest=org.freedesktop.Notifications \
+        --object-path=/org/freedesktop/Notifications \
+        --method=org.freedesktop.Notifications.Notify \
+        -- "" "0" "" "$1" "$2" "[]" "{}" "-1"
+}
+
 case $1 in
     add)
         clipboard="$(wl-paste -p)"
-        echo -e ""'"'"$clipboard"'"'"" >> ~/ankilist.txt
-        gdbus call --session \
-            --dest=org.freedesktop.Notifications \
-            --object-path=/org/freedesktop/Notifications \
-            --method=org.freedesktop.Notifications.Notify \
-            "" 0 "" "Ankilist" ""'"'"$clipboard"'"'" was added to ankilist!" \
-            '[]' '{"urgency": <1>}' 5000
-            ;;
+        echo -e ""'"'"$clipboard"'"'"" >>~/ankilist.txt
+        notify "Ankilist" ""'"'"$clipboard"'"'" was added to ankilist!"
+        ;;
 
     clear)
-        cat /dev/null > ~/ankilist.txt
-        gdbus call --session \
-            --dest=org.freedesktop.Notifications \
-            --object-path=/org/freedesktop/Notifications \
-            --method=org.freedesktop.Notifications.Notify \
-            "" 0 "" "Ankilist" "Ankilist has been cleared!" \
-            '[]' '{"urgency": <1>}' 5000
-            ;;
+        cat /dev/null >~/ankilist.txt
+        notify "Ankilist" "Ankilist has been cleared!"
+        ;;
 esac
